8267dada9db3d8dfa45ba502f4488508e8f77e2b
汪宇 <wangyu@fusionfintrade.com>
2021-12-01 17:23:44 +0800

    commit in 17

--- docs/gitalk-config.js
+++ docs/gitalk-config.js
@@ -1,12 +1,1 @@
-var gitalk = new Gitalk({
-    id: 'location.hash',
-    clientID: '05cb0f1a7375170c1450',
-    clientSecret: '096dd09023fdab671aa4b5bdd4a663d8e4837ca9',
-    repo: 'neet11.github.io',
-    owner: 'neet11',
-    admin: ['neet11'],
-    perPage: 50,
-    distractionFreeMode: false,
-})
-gitalk.render('gitalk-container');
-
+<script src="https://utteranc.es/client.js" repo="neet11/neet11.github.io" issue-term="pathname" theme="github-light" crossorigin="anonymous" async> </script>
