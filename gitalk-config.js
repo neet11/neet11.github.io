f4de19b8c24cb6b5e48bfdc02e5d28fb5e319ec2
汪宇 <wangyu@fusionfintrade.com>
2021-12-01 17:11:17 +0800

    commit in 17

--- gitalk-config.js
+++ gitalk-config.js
@@ -1,12 +1,25 @@
-var gitalk = new Gitalk({
-    id: 'location.hash',
-    clientID: '05cb0f1a7375170c1450',
-    clientSecret: '096dd09023fdab671aa4b5bdd4a663d8e4837ca9',
-    repo: 'neet11.github.io',
-    owner: 'neet11',
-    admin: ['neet11'],
-    perPage: 50,
-    distractionFreeMode: false,
-})
-gitalk.render('gitalk-container');
+// var gitalk = new Gitalk({
+    // id: 'location.hash',
+    // clientID: '05cb0f1a7375170c1450',
+    // clientSecret: '096dd09023fdab671aa4b5bdd4a663d8e4837ca9',
+    // repo: 'neet11.github.io',
+    // owner: 'neet11',
+    // admin: ['neet11'],
+    // perPage: 50,
+    // distractionFreeMode: false,
+// })
+// gitalk.render('gitalk-container');
+//
+// 加载的客户端脚本
+src="https://utteranc.es/client.js"
+// repo 就是访问的仓库，格式 用户名/仓库名
+// 个人就是 repo="wsafight/personBlog"
+repo="neet11/neet11.github.io"
 
+// 选定的当前blog 与 issue 之间的关系
+// 个人使用的是不会自动创建的 issue-number，每个issue都有自己的number。该选项是使用特定的issue
+issue-term="pathname"
+// 主题为  github-light 还有其他主题可以选择        
+theme="github-light"
+crossorigin="anonymous"
+        async>
