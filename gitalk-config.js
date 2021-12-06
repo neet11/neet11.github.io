41497aa3a753af663b01cc76bbb58fdb8fa1da14
汪宇 <wangyu@fusionfintrade.com>
2021-12-06 18:17:24 +0800

    commit in 17

--- gitalk-config.js
+++ gitalk-config.js
@@ -1,7 +1,7 @@
 var gitalk = new Gitalk({
     id: 'location.hash',
     clientID: '05cb0f1a7375170c1450',
-    clientSecret: '096dd09023fdab671aa4b5bdd4a663d8e4837ca9',
+    clientSecret: '0549cacd7883c2391f6ce5bc5d733453612d96ef',
     repo: 'neet11.github.io',
     id: window.location.pathname,
     owner: 'neet11',
