c4b2fbb9e3564673b7087e90ed0ec44f3673c6fe
neet11 <neetwy@163.com>
2022-10-03 17:41:16 +0800

    update pages

--- pushPage.sh
+++ pushPage.sh
@@ -4,12 +4,11 @@ cd "$(pwd)" || exit
 
 gitbook build
 
-# if [[ -d "./docs" ]];then
-#   rm -rf ./docs
-# fi
+if [[ -d "./docs" ]];then
+  rm -rf ./docs
+fi
 
-# cp -rf _book docs
-rsync -a _book docs
+mv _book docs
 
 git add --all
 git commit -m "update pages"
