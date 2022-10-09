9ffbfa850f1f6de26cb0e55c4c9266ffebd03be7
neet11 <neetwy@163.com>
2022-10-09 17:35:27 +0800

    update pages

--- pushPage.sh
+++ pushPage.sh
@@ -6,7 +6,7 @@
  # @Author       : neet11 neetwy@163.com
  # @Date         : 2022-09-26 13:08:04
  # @LastEditors  : neet11 neetwy@163.com
- # @LastEditTime : 2022-10-09 17:32:04
+ # @LastEditTime : 2022-10-09 17:35:00
  # @FilePath     : \neet11.github.io\pushPage.sh
 ### 
 
@@ -20,9 +20,9 @@ gitbook build
 
 mv _book docs
 
-#git add --all
-#git commit -m "update pages"
-#git push origin master --force
+git add --all
+git commit -m "update pages"
+git push origin master --force
 
 echo "============================"
 echo "git push github page success"
